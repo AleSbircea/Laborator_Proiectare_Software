a1766df8d1d6d0bbd44c1531d484e175dfbe95d2
AleSbircea <sbirceaalexandra7@gmail.com>
2026-03-15 21:34:18 +0200

    Rezolvare Laborator1 - clasa Student si instanta Student in main

--- src/Laborator2_Proiectare_software/MainLab2.java
+++ src/Laborator2_Proiectare_software/MainLab2.java
@@ -1,7 +1,55 @@
 package Laborator2_Proiectare_software;
 
+import java.util.*;
+
 public class MainLab2 {
     public static void main(String[] args){
+        List<Integer> x = new ArrayList();
+        List<Integer> y = new ArrayList();
+        List<Integer> xPlusY = new ArrayList(); //a
+        Set<Integer> zSet = new TreeSet(); //b
+        List<Integer> xMinusY = new ArrayList();//c
+        int p = 4;
+        List<Integer> xPlusYLimitedByP = new ArrayList(); //d
+
+        x.add(8);
+        x.add(1);
+        x.add(4);
+        x.add(9);
+        x.add(5);
+        y.add(2);
+        y.add(5);
+        y.add(3);
+        y.add(6);
+        y.add(9);
+        y.add(1);
+        y.add(7);
+
+        System.out.println(x.toString());
+        Collections.sort(x);
+        System.out.println(x.toString());
+        System.out.println();
+        System.out.println(y.toString());
+        Collections.sort(y);
+        System.out.println(y.toString());
+        System.out.println();
+
+        xPlusY.addAll(x);
+        xPlusY.addAll(y);
+        System.out.println(xPlusY);
+        Collections.sort(xPlusY);
+        System.out.println(xPlusY);
+        System.out.println();
+
+        zSet.addAll(x);
+        zSet.retainAll(y);
+        System.out.println(zSet);
+
+
+
+
+
+
 
     }
 }
