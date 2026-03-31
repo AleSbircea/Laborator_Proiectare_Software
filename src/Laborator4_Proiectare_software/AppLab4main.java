f8f2bae011a69330e9ad422a1e1a5f2f265123ef
AleSbircea <sbirceaalexandra7@gmail.com>
2026-03-31 20:30:47 +0300

    Pb 4.5.1- laborator 4

--- src/Laborator4_Proiectare_software/AppLab4main.java
+++ src/Laborator4_Proiectare_software/AppLab4main.java
@@ -10,6 +10,7 @@ import java.util.TreeMap;
 import java.util.WeakHashMap;
 
 public class AppLab4main {
+
     public static void main(String[] args) {
         //pb 4.5.1
         HashMap<String, Integer> varste = new HashMap<>();
@@ -22,12 +23,22 @@ public class AppLab4main {
         varste.put("Daniela", 23);
         Map<String, String> adrese = Map.of("Ioan", "Sibiu", "Maria", "Bucuresti", "Victor",
                 "Cluj", "Simina", "Alba-Iulia", "Marius", "Medias", "Mihai", "Cisnadie", "Daniela", "Sibiu");
-        HashMap<String, Tanar> tineri;
 
         System.out.println("Se afiseaza continutul instantei varsta initiala:" + varste);
         varste.put("Vlad",19);
         varste.put("Iulia",19);
         System.out.println("Se afiseaza continutul instantei varsta dupa adaugare:" + varste);
 
+        HashMap<String, Tanar> tineri=new HashMap<>();
+        for (String nume : varste.keySet()) {
+            int varsta = varste.get(nume);
+            String adresa = adrese.get(nume);
+            Tanar persoana = new Tanar(nume, varsta, adresa);
+            tineri.put(nume, persoana);
+        }
+
+            System.out.println("Se afiseaza continutul instantei tineri: "+ tineri);
+
     }
+
 }
