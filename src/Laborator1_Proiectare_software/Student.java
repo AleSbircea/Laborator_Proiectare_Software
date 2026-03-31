05b9c397dbe77c52802a9946cbb1ecf615ef3abf
AleSbircea <sbirceaalexandra7@gmail.com>
2026-03-31 21:56:51 +0300

    Pb 4.5.2- laborator 4

--- src/Laborator1_Proiectare_software/Student.java
+++ src/Laborator1_Proiectare_software/Student.java
@@ -7,23 +7,29 @@ public class Student {
     protected String prenume;
     protected String nume;
     protected String formatieDeStudiu;
-    public Student(int numarMatricol,String prenume, String nume, String  formatieStudiu){
-        this.numarMatricol=numarMatricol;
-        this.prenume=prenume;
-        this.nume=nume;
-        this.formatieDeStudiu=formatieStudiu;
+    protected double note;
+
+    public Student(int numarMatricol, String prenume, String nume, String formatieStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieStudiu;
+        this.note = 1;
     }
+
     @Override
-    public String toString(){
-        return "Student: " + numarMatricol+ ", "  + prenume + ", "  + nume + ", " + formatieDeStudiu+";";
+    public String toString() {
+        return "Student: " + numarMatricol + ", " + prenume + ", " + nume + ", " + formatieDeStudiu + "," + note +";";
     }
-    public boolean sameStudent(Student student){
-        if(this.numarMatricol==student.numarMatricol && this.prenume.equals(student.prenume) && this.nume.equals(student.nume)&& this.formatieDeStudiu.equals(student.formatieDeStudiu)){
+
+    public boolean sameStudent(Student student) {
+        if (this.numarMatricol == student.numarMatricol && this.prenume.equals(student.prenume) && this.nume.equals(student.nume) && this.formatieDeStudiu.equals(student.formatieDeStudiu)) {
             return true;
         }
         return false;
     }
-//adaugare lab2
+
+    //adaugare lab2
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -32,24 +38,36 @@ public class Student {
         return Objects.equals(numarMatricol, student.numarMatricol) &&
                 Objects.equals(nume, student.nume) &&
                 Objects.equals(prenume, student.prenume) &&
-                Objects.equals(formatieDeStudiu, student.formatieDeStudiu);
+                Objects.equals(formatieDeStudiu, student.formatieDeStudiu)&&
+                Objects.equals(note, student.note);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(numarMatricol, nume, prenume, formatieDeStudiu);
+        return Objects.hash(numarMatricol, nume, prenume, formatieDeStudiu,note);
     }
-//adaugare lab3
+
+    //adaugare lab3
     public String getNume() {
         return nume;
     }
+
     public String getPrenume() {
         return prenume;
     }
+
     public int getNumarMatricol() {
         return numarMatricol;
     }
+
     public String getFormatieDeStudiu() {
         return formatieDeStudiu;
     }
+
+    //aduagare lab4 tot ce tine de note
+    public void setNote(double note) {
+        this.note = note;
+
+    }
 }
+
--- src/Laborator4_Proiectare_software/AppLab4main.java
+++ src/Laborator4_Proiectare_software/AppLab4main.java
@@ -1,15 +1,17 @@
 package Laborator4_Proiectare_software;
+import Laborator1_Proiectare_software.Student;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Hashtable;
-import java.util.EnumMap;
-import java.util.IdentityHashMap;
-import java.util.LinkedHashMap;
-import java.util.TreeMap;
-import java.util.WeakHashMap;
+
 
 public class AppLab4main {
+
 
     public static void main(String[] args) {
         //pb 4.5.1
@@ -25,11 +27,11 @@ public class AppLab4main {
                 "Cluj", "Simina", "Alba-Iulia", "Marius", "Medias", "Mihai", "Cisnadie", "Daniela", "Sibiu");
 
         System.out.println("Se afiseaza continutul instantei varsta initiala:" + varste);
-        varste.put("Vlad",19);
-        varste.put("Iulia",19);
+        varste.put("Vlad", 19);
+        varste.put("Iulia", 19);
         System.out.println("Se afiseaza continutul instantei varsta dupa adaugare:" + varste);
 
-        HashMap<String, Tanar> tineri=new HashMap<>();
+        HashMap<String, Tanar> tineri = new HashMap<>();
 
         for (String nume : varste.keySet()) {
             int varsta = varste.get(nume);
@@ -38,8 +40,63 @@ public class AppLab4main {
             tineri.put(nume, persoana);
         }
 
-            System.out.println("Se afiseaza continutul instantei tineri: "+ tineri);
+        System.out.println("Se afiseaza continutul instantei tineri: " + tineri);
+
+
+        //pb 4.5.2
+        HashMap<String, Student> students = new HashMap<>();
+        try {
+            citireStudenti("studenti_out_sorted.txt",students);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            citireNoteStudenti("note_anon.txt", students);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-}
+
+    public static void citireStudenti(String fileName, HashMap<String, Student> students) throws IOException {
+        System.out.println("Using Files.readAllLines:");
+        Path path = Paths.get(fileName);
+        List<String> allLinesInMemory = Files.readAllLines(path);
+        for (String line : allLinesInMemory) {
+            String[] date = line.split(",");
+
+            if (date.length >= 4) {
+                int nrMatricol = Integer.parseInt(date[0].trim());
+                String nume = date[1];
+                String prenume = date[2];
+                String grupa = date[3];
+                Student s = new Student(nrMatricol, nume, prenume, grupa);
+                students.put(date[0].trim(), s);
+            }
+
+        }
+        System.out.println(students);
+    }
+        public static void citireNoteStudenti (String fileName, HashMap < String, Student > students) throws IOException
+        {
+            System.out.println("Using Files.readAllLines:");
+            Path path = Paths.get(fileName);
+            List<String> allLinesInMemory = Files.readAllLines(path);
+            for (String line : allLinesInMemory) {
+                String[] date = line.split(",");
+                if (date.length == 2) {
+                    String nrMatricol = date[0];
+                    double nota = Double.parseDouble(date[1]);
+                    Student s = students.get(nrMatricol);
+                    if (s != null) {
+                        s.setNote(nota);
+                    }
+                }
+
+            }
+            System.out.println(students);
+        }
+    }
+
+
